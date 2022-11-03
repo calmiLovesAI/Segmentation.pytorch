cb59e1d4fa37182aa593af962d07808f3f6d2777
calmisential <xinyu_std@163.com>
2022-11-03 10:15:55 +0800

    Update

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import cv2
 import torch
-import torchvision
+import torchvision.transforms.functional as F
 
 from core.parse import update_cfg
 from core.post_process import PostProcess
@@ -31,14 +31,20 @@ def main():
         image_name = Path(image_path).stem
         print(f"Reading image: {image_path}")
 
-        img = torchvision.transforms.functional.to_tensor(cv2_read_image(img))
+        img = F.to_tensor(cv2_read_image(img))
         # resize to fixed input size
         # input_size = cfg["Train"]["input_size"][1:]
-        # img = torchvision.transforms.functional.resize(img, size=input_size)
+        # original_size = [img.size(1), img.size(2)]
+        # img = F.resize(img, size=input_size)
 
         with torch.no_grad():
             result = PostProcess(img, model, device).fcn()
+
         result = torch.squeeze(result, dim=0)
+        # result = torch.permute(result, dims=[2, 0, 1])   # (h, w, c) -> (c, h, w)
+        # # back to original size
+        # result = F.resize(result, size=original_size)
+        # result = torch.permute(result, dims=[1, 2, 0])   # (c, h, w) -> (h, w, c)
         result = result.cpu().numpy()
         # rgb -> bgr
         result = result[..., ::-1]
