bceb533c8c4f268609cfa11deadea648f4359a75
calmisential <xinyu_std@163.com>
2022-11-15 15:23:34 +0800

    优化代码

--- core/parse.py
+++ core/parse.py
@@ -5,30 +5,44 @@ from models.fcn import FCN
 from models.unet import UNet
 
 
+def get_model(cfg: dict, model_name: str):
+    match model_name:
+        case "FCN":
+            return FCN(num_classes=cfg["Dataset"]["num_classes"] + 1)
+        case "UNet":
+            model_cfg = cfg["Model"]
+            return UNet(num_classes=cfg["Dataset"]["num_classes"] + 1,
+                        in_channels=model_cfg["Up"]["in_channels"],
+                        out_channels=model_cfg["Up"]["out_channels"],
+                        pretrained=model_cfg["backbone"]["pretrained"])
+
+
+def get_dataset(cfg: dict, dataset_name: str):
+    match dataset_name:
+        case "VOC":
+            train_dataloader = get_voc_dataloader(cfg, True)
+            valid_dataloader = get_voc_dataloader(cfg, False)
+
+    return train_dataloader, valid_dataloader
+
+
+def update_hyperparams(cfg: dict, opts):
+    cfg["Train"]["batch_size"] = opts.batch_size
+    cfg["Train"]["start_epoch"] = opts.start_epoch
+    cfg["Train"]["epochs"] = opts.epochs
+    cfg["Train"]["learning_rate"] = opts.lr
+    cfg["Train"]["save_frequency"] = opts.save_freq
+    cfg["Train"]["tensorboard_on"] = opts.tensorboard
+
+
 def update_cfg(cfg: dict, opts, device, use_dataset=True) -> dict:
     cfg.update({"device": device})
     if opts is not None:
         # update some training hyperparameters
-        cfg["Train"]["batch_size"] = opts.batch_size
-        cfg["Train"]["start_epoch"] = opts.start_epoch
-        cfg["Train"]["epochs"] = opts.epochs
-        cfg["Train"]["learning_rate"] = opts.lr
-        cfg["Train"]["save_frequency"] = opts.save_freq
-        cfg["Train"]["tensorboard_on"] = opts.tensorboard
-    if cfg["Model"]["name"] == "FCN":
-        fcn = FCN(num_classes=cfg["Dataset"]["num_classes"] + 1)
-        cfg.update({"model": fcn})
-    elif cfg["Model"]["name"] == "UNet":
-        model_cfg = cfg["Model"]
-        unet = UNet(num_classes=cfg["Dataset"]["num_classes"] + 1,
-                    in_channels=model_cfg["Up"]["in_channels"],
-                    out_channels=model_cfg["Up"]["out_channels"],
-                    pretrained=model_cfg["backbone"]["pretrained"])
-        cfg.update({"model": unet})
+        update_hyperparams(cfg, opts)
+    cfg.update({"model": get_model(cfg=cfg, model_name=cfg["Model"]["name"])})
     if use_dataset:
-        if cfg["Dataset"]["name"] == "VOC":
-            train_dataloader = get_voc_dataloader(cfg, True)
-            valid_dataloader = get_voc_dataloader(cfg, False)
-            cfg.update({"train_dataloader": train_dataloader})
-            cfg.update({"valid_dataloader": valid_dataloader})
+        train_dataloader, valid_dataloader = get_dataset(cfg=cfg, dataset_name=cfg["Dataset"]["name"])
+        cfg.update({"train_dataloader": train_dataloader})
+        cfg.update({"valid_dataloader": valid_dataloader})
     return cfg
